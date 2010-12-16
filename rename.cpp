95d33eaae00203abca785e7c611eaa4c9270f204
Pali Rohár <pali.rohar@gmail.com>
2010-12-16 23:22:14 +0100

    Implemented function rename

--- rename.cpp
+++ rename.cpp
@@ -1,5 +1,84 @@
+/*
+    rename.cpp - StormLib MPQ archiving utility
+    Copyright (C) 2010  Pali Rohár <pali.rohar@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <StormLib.h>
+
+///
 extern "C" {
-#include <time.h>
+///
+
 #include "common.h"
-int rename(const char * archive, const char * oldName, const char * newName, int flags, const char * listfile, int locale) {}
+
+int rename(const char * archive, const char * oldName, const char * newName, int flags, const char * listfile, int locale) {
+
+	HANDLE SArchive = NULL;
+
+	int SFlags = 0;
+
+	if ( flags & NO_LISTFILE )
+		SFlags |= MPQ_OPEN_NO_LISTFILE;
+
+	if ( flags & NO_ATTRIBUTES )
+		SFlags |= MPQ_OPEN_NO_ATTRIBUTES;
+
+	if ( flags & MPQ_VERSION_1 )
+		SFlags |= MPQ_OPEN_FORCE_MPQ_V1;
+
+	if ( flags & SECTOR_CRC )
+		SFlags |= MPQ_OPEN_CHECK_SECTOR_CRC;
+
+	if ( ! SFileOpenArchive(archive, 0, SFlags, &SArchive) ) {
+
+		if ( ! ( flags & QUIET ) )
+			printError(archive, "Cannot open archive", archive, GetLastError());
+
+		return -1;
+
+	}
+
+	if ( ! ( flags & NO_SYSTEM_LF ) )
+		systemListfiles(SArchive, archive, flags);
+
+	SFileAddListFile(SArchive, listfile);
+
+	SFileSetLocale(locale);
+
+	if ( flags & VERBOSE )
+		printVerbose(archive, "Rename file", oldName);
+
+	if ( ! SFileRenameFile(SArchive, oldName, newName) ) {
+
+		if ( ! ( flags & QUIET ) )
+			printError(archive, "Cannot rename file", oldName, GetLastError());
+
+		SFileCloseArchive(SArchive);
+
+		return -1;
+
+	}
+
+	SFileCloseArchive(SArchive);
+
+	return 0;
+
 }
+
+///
+}
+///
