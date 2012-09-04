7a0216ac4f9d9e8ac54b529821043361a9af1dc6
Pali Rohár <pali.rohar@gmail.com>
2012-09-04 17:11:27 +0200

    Use build date instead hardcoded in man page

--- mangen.c
+++ mangen.c
@@ -40,7 +40,7 @@ int main(void) {
 	const char * end = start;
 	const char * len = start + strlen(HELP) + 1;
 
-	printf(".TH SMPQ 1 \"Oct 2011\" \"SMPQ - StormLib MPQ archiving utility, version " VERSION "\"\n");
+	printf(".TH SMPQ 1 \"" __DATE__ "\" \"SMPQ - StormLib MPQ archiving utility, version " VERSION "\"\n");
 
 	while ( start < len && ( end = strchr(start, '\n') ) != NULL ) {
 
